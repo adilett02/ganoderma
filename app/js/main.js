0f39811f1682c623c8ba73d7f5e9dc4172e64216
adilett02 <adiletturdakunov2002@gmail.com>
2021-01-11 12:47:06 +0600

    mobile test

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,42 @@
 $(function () {
-    var siteScroll = function() {	
+    var siteScroll = function () {
 
-        $(window).scroll(function() {
-  
+        $(window).scroll(function () {
+
             var st = $(this).scrollTop();
-  
+
             if (st > 50) {
                 $('.header').addClass('scroll');
             } else {
                 $('.header').removeClass('scroll');
+                $('.navbar-toggler').on('click', function () {
+                    $('.header').toggleClass('scroll');
+                    // if ($('.header').hasClass('scroll')) {
+                    //     $('.header').toggleClass('scroll');
+                    // } else {
+                    //     $('.header').toggleClass('scroll');
+                    // }
+                });
             }
-  
-        }) 
-  
+
+        })
+
     };
     siteScroll();
     AOS.init({
-         duration: 800
-    }   
+        duration: 800
+    }
     );
+
+
+
+    // $('.navbar-toggler').on('click', function () {
+    //     // $('.header').toggleClass('scroll');
+    //     if ($('.header').hasClass('scroll')) {
+    //         // $('.header').removeClass('scroll');
+    //         $('.header').toggleClass('scroll');
+    //     } else {
+    //         $('.header').toggleClass('scroll');
+    //     }
+    // });
 })
