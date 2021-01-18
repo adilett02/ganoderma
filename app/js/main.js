4ba39a5b397a4d280d663031bbd46271dc234e72
adilett02 <adiletturdakunov2002@gmail.com>
2021-01-18 17:57:28 +0600

    monday

--- app/js/main.js
+++ app/js/main.js
@@ -103,4 +103,14 @@ $(function ($) {
             prevEl: '.swiper-button-prev',
         },
     })
+
+    //Индикатор прокрутки
+    window.onscroll = function () { myFunction() };
+
+    function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }
 });
