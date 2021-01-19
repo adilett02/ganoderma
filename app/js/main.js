03252b193d26918bfeb11c711f8dfa52f034d64d
adilett02 <adiletturdakunov2002@gmail.com>
2021-01-19 16:52:15 +0600

    back to top

--- app/js/main.js
+++ app/js/main.js
@@ -113,4 +113,18 @@ $(function ($) {
         var scrolled = (winScroll / height) * 100;
         document.getElementById("myBar").style.width = scrolled + "%";
     }
+
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+        } else {
+          $('.back-to-top').fadeOut('slow');
+        }
+      });
+      $('.back-to-top').on('click', function() {
+        $('html, body').animate({scrollTop : 0},1000);
+        return false;
+      })
 });
