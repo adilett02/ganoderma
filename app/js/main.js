23c88a0b728bbeb478492fc0314197d3161c49b1
adilett02 <adiletturdakunov2002@gmail.com>
2021-02-17 17:35:12 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -193,21 +193,16 @@
     siteMenuClone();
 
 
-    // var siteSticky = function () {
-    //     $(".js-sticky-header").sticky({ topSpacing: 0 });
-    // };
-    // siteSticky();
+    var siteSticky = function () {
+        $(".js-sticky-header").sticky({ topSpacing: 0 });
+    };
+    siteSticky();
 
 
 
 
 
     //typed js
-    // var typed = new Typed('.subtitle', {
-    //     strings: ["First sentence.", "Международная компания SmartRich предлагает Вам оценить уникальный вкус кофе и чая!"],
-    //     typeSpeed: 40,
-    //     showCursor: false,
-    // });
     new TypeIt(".subtitle", {
         // strings: "Международная компания Smar&tRich предлагает Вам оценить уникальный вкус кофе и чая!",
         speed: 60,
